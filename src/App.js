73812b6b65d34a0f3ce6c4de018276838feb574d
vanestle <vanyssachan@gmail.com>
2026-03-30 10:31:24 +0700

    Add react-jsx-lab files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Item from './Item';
 
 function App() {
+  const name = "John Doe";
+  const items = ['Item 1', 'Item 2', 'Item 3'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, {name}!</h1>
+      <p>Welcome to React with JSX.</p>
+      <h2>My Items:</h2>
+      <ul>
+        {items.map((item, index) => (
+          <Item key={index} name={item} />
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/Item.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Item(props) {
+  return <li>{props.name}</li>;
+}
+
+export default Item;
